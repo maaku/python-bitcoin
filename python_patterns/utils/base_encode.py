cdeae1cf7baae116c032f6630101402c4ecb51f4
Mark Friedenbach <mark@roku-sigma.com>
2011-09-06 14:04:52 -0700

    Added base encoding and decoding routines in python_patterns.utils.base_encode.

---
+++ python_patterns/utils/base_encode.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# === python_patterns.utils.base_encode -----------------------------------===
+# Copyright © 2011, RokuSigma Inc. (Mark Friedenbach <mark@roku-sigma.com>)
+# as an unpublished work.
+#
+# RokuSigma Inc. (the “Company”) Confidential
+#
+# NOTICE: All information contained herein is, and remains the property of the
+# Company. The intellectual and technical concepts contained herein are
+# proprietary to the Company and may be covered by U.S. and Foreign Patents,
+# patents in process, and are protected by trade secret or copyright law.
+# Dissemination of this information or reproduction of this material is
+# strictly forbidden unless prior written permission is obtained from the
+# Company. Access to the source code contained herein is hereby forbidden to
+# anyone except current Company employees, managers or contractors who have
+# executed Confidentiality and Non-disclosure agreements explicitly covering
+# such access.
+#
+# The copyright notice above does not evidence any actual or intended
+# publication or disclosure of this source code, which includes information
+# that is confidential and/or proprietary, and is a trade secret, of the
+# Company. ANY REPRODUCTION, MODIFICATION, DISTRIBUTION, PUBLIC PERFORMANCE,
+# OR PUBLIC DISPLAY OF OR THROUGH USE OF THIS SOURCE CODE WITHOUT THE EXPRESS
+# WRITTEN CONSENT OF THE COMPANY IS STRICTLY PROHIBITED, AND IN VIOLATION OF
+# APPLICABLE LAWS AND INTERNATIONAL TREATIES. THE RECEIPT OR POSSESSION OF
+# THIS SOURCE CODE AND/OR RELATED INFORMATION DOES NOT CONVEY OR IMPLY ANY
+# RIGHTS TO REPRODUCE, DISCLOSE OR DISTRIBUTE ITS CONTENTS, OR TO MANUFACTURE,
+# USE, OR SELL ANYTHING THAT IT MAY DESCRIBE, IN WHOLE OR IN PART.
+# ===----------------------------------------------------------------------===
+
+ALPHABET_LIST = u"abcdefghjkmnpqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ23456789-"
+ALPHABET_DICT = dict((c, i) for i, c in enumerate(ALPHABET_LIST))
+ALPHABET_LENGTH = len(ALPHABET_LIST)
+
+def base_encode(num, base=None, little_endian=False):
+  """
+  Encodes the passed-in number into a textual representation using the passed-
+  in alphabet, or a default alphabet of 56 easily distinguishable characters.
+
+    num
+      The number to encode.
+
+    base
+      A string containing the list of base characters to be used, in order
+      (default: "abcdefghjkmnpqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ23456789-").
+
+    little_endian
+      Boolean value specifying whether the output should be little-endian or
+      big-endian (default: False).
+  """
+  if num < 0:
+    raise ValueError, "cannot base encode negative number: %d" % num
+  if base is None:
+    base   = ALPHABET_LIST
+    length = ALPHABET_LENGTH
+  else:
+    base   = unicode(base)
+    length = len(base)
+  ret = ''
+  if num == 0:
+    return base[0]
+  while num != 0:
+    if little_endian:
+      ret = ret + base[num % length]
+    else:
+      ret = base[num % length] + ret
+    num /= length
+  return ret
+
+def base_decode(string, base=None, little_endian=False):
+  """
+  Decodes the passed-in string (presumably created with base_encode) from a
+  textual representation to a number.
+
+    string
+      The encoded number.
+
+    base
+      A string containing the list of base characters to be used, in order
+      (default: "abcdefghjkmnpqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ23456789-").
+
+    little_endian
+      Boolean value specifying whether the output should be little-endian or
+      big-endian (default: False).
+  """
+  if base is None:
+    reverse_base = ALPHABET_DICT
+    length       = ALPHABET_LENGTH
+  else:
+    reverse_base = dict((c, i) for i, c in enumerate(unicode(base)))
+    length       = len(base)
+  ret = 0
+  if little_endian:
+    for i, c in enumerate(string):
+      ret += (length ** i) * reverse_base[c]
+  else:
+    for i, c in enumerate(string[::-1]):
+      ret += (length ** i) * reverse_base[c]
+  return ret
+
+# ===----------------------------------------------------------------------===
+# End of File
+# ===----------------------------------------------------------------------===
---
+++ python_patterns/utils/base_encode_test.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# === python_patterns.utils.base_encode_test ------------------------------===
+# Copyright © 2011, RokuSigma Inc. (Mark Friedenbach <mark@roku-sigma.com>)
+# as an unpublished work.
+#
+# RokuSigma Inc. (the “Company”) Confidential
+#
+# NOTICE: All information contained herein is, and remains the property of the
+# Company. The intellectual and technical concepts contained herein are
+# proprietary to the Company and may be covered by U.S. and Foreign Patents,
+# patents in process, and are protected by trade secret or copyright law.
+# Dissemination of this information or reproduction of this material is
+# strictly forbidden unless prior written permission is obtained from the
+# Company. Access to the source code contained herein is hereby forbidden to
+# anyone except current Company employees, managers or contractors who have
+# executed Confidentiality and Non-disclosure agreements explicitly covering
+# such access.
+#
+# The copyright notice above does not evidence any actual or intended
+# publication or disclosure of this source code, which includes information
+# that is confidential and/or proprietary, and is a trade secret, of the
+# Company. ANY REPRODUCTION, MODIFICATION, DISTRIBUTION, PUBLIC PERFORMANCE,
+# OR PUBLIC DISPLAY OF OR THROUGH USE OF THIS SOURCE CODE WITHOUT THE EXPRESS
+# WRITTEN CONSENT OF THE COMPANY IS STRICTLY PROHIBITED, AND IN VIOLATION OF
+# APPLICABLE LAWS AND INTERNATIONAL TREATIES. THE RECEIPT OR POSSESSION OF
+# THIS SOURCE CODE AND/OR RELATED INFORMATION DOES NOT CONVEY OR IMPLY ANY
+# RIGHTS TO REPRODUCE, DISCLOSE OR DISTRIBUTE ITS CONTENTS, OR TO MANUFACTURE,
+# USE, OR SELL ANYTHING THAT IT MAY DESCRIBE, IN WHOLE OR IN PART.
+# ===----------------------------------------------------------------------===
+
+# Python standard library, unit-testing
+import unittest2
+
+# Python patterns, scenario unit-testing
+from python_patterns.unittest.scenario import ScenarioMeta, ScenarioTest
+
+# Python patterns, base encoding
+from python_patterns.utils.base_encode import base_encode, base_decode
+
+SCENARIOS = [
+  dict(num=0, string='a'),
+  dict(num=1, string='b'),
+  dict(num=2, string='c'),
+  dict(num=54, string='9'),
+  dict(num=55, string='-'),
+  dict(num=56, string='ba'),
+  dict(num=57, string='bb'),
+  dict(num=96**34, string='kq6QFsNzjZC6JCmn7ngcgLAjVvsh-yCKt2qFMMt'),
+  dict(base="123", num=0, string='1'),
+  dict(base="123", num=1, string='2'),
+  dict(base="123", num=2, string='3'),
+  dict(base="123", num=3, string='21'),
+  dict(base="123", num=4, string='22'),
+  dict(base=u"éü", num=0, string=u'é'),
+  dict(base=u"éü", num=1, string=u'ü'),
+  dict(base=u"éü", num=2, string=u'üé'),
+  dict(num=100, string='bX', little_endian=False),
+  dict(num=100, string='Xb', little_endian=True),
+]
+
+class TestBaseEncode(unittest2.TestCase):
+  """
+  Test encoding and decoding using a variety of standard scenarios.
+  """
+  __metaclass__ = ScenarioMeta
+  class test_encode(ScenarioTest):
+    scenarios = SCENARIOS
+    def __test__(self, num, string, base=None, little_endian=False):
+      self.assertEqual(base_encode(num, base=base, little_endian=little_endian), string)
+  class test_decode(ScenarioTest):
+    scenarios = SCENARIOS
+    def __test__(self, num, string, base=None, little_endian=False):
+      self.assertEqual(num, base_decode(string, base=base, little_endian=little_endian))
+
+class TestNegativeNumberEncode(unittest2.TestCase):
+  """
+  Test that encoding a negative number results in a value error.
+  """
+  def test_negative_number(self):
+    self.assertRaises(ValueError, base_encode, (-1))
+
+# ===----------------------------------------------------------------------===
+# End of File
+# ===----------------------------------------------------------------------===
