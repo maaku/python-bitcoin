b348df273fc022d14ad7696538ee855dd8a55745
Mark Friedenbach <mark@friedenbach.org>
2012-09-23 02:24:34 -0700

    Add MIT license from pynode.

--- bitcoin/serialization.py
+++ bitcoin/serialization.py
@@ -30,6 +30,34 @@
 # USE, OR SELL ANYTHING THAT IT MAY DESCRIBE, IN WHOLE OR IN PART.
 # ===----------------------------------------------------------------------===
 
+# ===----------------------------------------------------------------------===
+# This file is based in part on code published as part of the pynode project.
+# The original work is copyrighted by it's authors and licensed according to
+# the terms of the MIT license (below); modifications since then are the
+# property of RokuSigma Inc. and subject to the above-referenced
+# confidentiality and non-disclosure agreements.
+# ===----------------------------------------------------------------------===
+
+# ===----------------------------------------------------------------------===
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+# ===----------------------------------------------------------------------===
+
 "Utility functions used in implementing the block chain serialization format."
 
 from struct import pack, unpack
