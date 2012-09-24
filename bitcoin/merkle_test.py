378545cdf8b6ba784c8105f542ea321636b40a1a
Mark Friedenbach <mark@friedenbach.org>
2012-09-24 14:52:47 -0700

    Added unit tests for Merkle-root calculation, and fix to allow for empty parameters (resulting in the zero hash).

--- bitcoin/merkle_test.py
+++ bitcoin/merkle_test.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# === bitcoin.merkle ------------------------------------------------------===
+# === bitcoin.merkle_test -------------------------------------------------===
 # Copyright © 2012, RokuSigma Inc. and contributors as an unpublished work.
 # See AUTHORS for details.
 #
@@ -58,18 +58,36 @@
 # THE SOFTWARE.
 # ===----------------------------------------------------------------------===
 
-from itertools import izip
+# Python standard library, unit-testing
+import unittest2
 
-from .crypto import hash256
-from .serialize import serialize_hash
+# Python patterns, scenario unit-testing
+from python_patterns.unittest.scenario import ScenarioMeta, ScenarioTest
 
-def merkle_list(hashes):
-    hashes = list(hashes)
-    while len(hashes) > 1:
-        hashes = map(lambda h:serialize_hash(h, 32), hashes)
-        hashes.append(hashes[-1])
-        hashes = [hash256(l+r) for l,r in izip(*[iter(hashes)]*2)]
-    return hashes and hashes[0] or 0L
+from .merkle import merkle_list
+
+MERKLE_LIST = [
+    dict(list_=[], merkle=0),
+    dict(list_=[0x4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33bL],
+         merkle=0x4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33bL),
+    dict(list_=[0x50671f9e3b0719fcd23b2976eedf6e0a6f19ae7a8f023c3e66b9b1fbe207a006L,
+                0x603b9dbb388987175f8ddbdae9a8ad77a999e63b356e6172b331905387b515f7L,
+                0xec258956c3069ea974a569fbacd320fd183d65fa6d4c318ad4e55da6260c0f3dL],
+         merkle=0x5d438b61fa23d20bfac0a6eaf5c426e2abdd48637c7dd70a620b0b7ab7cd6092L),
+    dict(list_=[0x9156235baa55ea841c4384faa543004fd5a56661d22dfbc2bbd5313c85044e08L,
+                0xc6f50393c676f1f3b669c93f62c3f0e46349f6d2aff13ed8976bd649382ff6fcL,
+                0x4817fad15ce7604ae5a478888a29b4ade8000596a0e9267747b1d4f0bd1a6defL,
+                0x16952eba67618e90857d530a63477ea09dd89575ac00c114e20e8068b4e0297cL],
+         merkle=0xb1cd750f2a242deeaf3b452aa69a4a46f9f66e866c07eda22677f1287688bb1eL),
+]
+
+class TestMerkleList(unittest2.TestCase):
+    "Test merkle_list API under a variety of scenarios."
+    __metaclass__ = ScenarioMeta
+    class test_merkle_root(ScenarioTest):
+        scenarios = MERKLE_LIST
+        def __test__(self, list_, merkle):
+            self.assertEqual(merkle_list(list_), merkle)
 
 # ===----------------------------------------------------------------------===
 # End of File
