bb1bc925a20bdd3a9d1b16caea763c3a0a32c6f3
Mark Friedenbach <mark@friedenbach.org>
2013-01-23 14:59:41 -0800

    Hey look, it's 2013.

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 # === makefile ------------------------------------------------------------===
-# Copyright © 2012 by its contributors. See AUTHORS for details.
+# Copyright © 2012-2013 by its contributors. See AUTHORS for details.
 #
 # Distributed under the MIT/X11 software license, see the accompanying
 # file LICENSE or http://www.opensource.org/licenses/mit-license.php.
--- tests/crypto.py
+++ tests/crypto.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 # === bitcoin.crypto_test -------------------------------------------------===
-# Copyright © 2012 by its contributors. See AUTHORS for details.
+# Copyright © 2012-2013 by its contributors. See AUTHORS for details.
 #
 # Distributed under the MIT/X11 software license, see the accompanying
 # file LICENSE or http://www.opensource.org/licenses/mit-license.php.
