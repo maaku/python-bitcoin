c0beaddf3c3043d393642d0872ce0e8bb44f2c16
Mark Friedenbach <mark@friedenbach.org>
2013-02-16 21:09:17 -0800

    Remove spaces from version string.

--- bitcoin/__init__.py
+++ bitcoin/__init__.py
@@ -15,10 +15,10 @@ def get_version():
     if VERSION[2]:
         version = '%s.%s' % (version, VERSION[2])
     if VERSION[3:] == ('alpha', 0):
-        version = '%s pre-alpha' % version
+        version = '%spre-alpha' % version
     else:
         if VERSION[3] != 'final':
-            version = "%s %s" % (version, VERSION[3])
+            version = "%s%s" % (version, VERSION[3])
             if VERSION[4] != 0:
                 version = '%s%s' % (version, VERSION[4])
     return version
