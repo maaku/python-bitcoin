f5d8ccb8e25fe50c3594e00a00c81f1b941c78b9
Mark Friedenbach <mark@friedenbach.org>
2013-07-20 17:01:50 -0700

    Add UnspentTransaction, a pythonic version of CCoins from the ultraprune branch of bitcoind.

---
+++ bitcoin/utxo.py
@@ -0,0 +1,227 @@
+# -*- coding: utf-8 -*-
+
+#
+# Copyright © 2012-2013 by its contributors. See AUTHORS for details.
+#
+# Distributed under the MIT/X11 software license, see the accompanying
+# file LICENSE or http://www.opensource.org/licenses/mit-license.php.
+#
+
+# Python 2 and 3 compatibility utilities
+import six
+
+from blist import sorteddict
+
+from .core import Output
+from .mixins import SerializableMixin
+from .serialize import (
+    serialize_leint, deserialize_leint,
+    serialize_varint, deserialize_varint)
+from .utils import compress_amount, decompress_amount
+
+# ===----------------------------------------------------------------------===
+
+from .script import ScriptPickler
+
+class UnspentTransaction(SerializableMixin, sorteddict):
+    """Pruned version of core.Transaction: only retains metadata and unspent
+    transaction outputs.
+
+    Serialized format:
+        - VARINT(version)
+        - VARINT(code)
+        - unspentness bitvector, for outputs[2] and further; least significant
+          byte first
+        - the non-spent, compressed TransactionOutputs
+        - VARINT(height)
+        - VARINT(reference_height)
+
+    The code value consists of:
+        - bit 1: is_coinbase flag
+        - bit 2: outputs[0] is not spent
+        - bit 4: outputs[1] is not spent
+        - The higher bits encode N, the number of non-zero bytes in the following
+          bitvector.
+            - In case both bit 2 and bit 4 are unset, they encode N-1, as there
+              must be at least one non-spent output.
+
+    Example: 0104835800816115944e077fe7c803cfa57f29b36bf87c1d358bb85e
+             <><><--------------------------------------------><---->
+             |  \                  |                             /
+       version   code           outputs[1]                  height
+    
+        - version = 1
+        - code = 4 (outputs[1] is not spent, and 0 non-zero bytes of bitvector follow)
+        - unspentness bitvector: as 0 non-zero bytes follow, it has length 0
+        - outputs[1]: 835800816115944e077fe7c803cfa57f29b36bf87c1d35
+            * 8358: compact amount representation for 60000000000 (600 BTC)
+            * 00: special txout type pay-to-pubkey-hash
+            * 816115944e077fe7c803cfa57f29b36bf87c1d35: address uint160
+        - height = 203998
+    
+     Example: 02090440fe792b067e0061b01caab50f1b8e9c50a5057eb43c2d9563a4ee...
+              <><><--><-------------------------------------------------->
+             /  |   \                     |
+      version  code  unspentness     outputs[4]
+
+              ...fe23290f00008c988f1a4a4de2161e0f50aac7f17e7f9555caa4fe3bd80100fde803
+                 <--------------------------------------------------><--------><---->
+                                             |                         /          |
+                                        outputs[16]               height  reference_height
+    
+      - version = 2
+      - code = 9: coinbase, neither outputs[0] or outputs[1] are unspent,
+        2 (1, +1 because both bit 2 and bit 4 are unset) non-zero bitvector
+        bytes follow.
+      - unspentness bitvector: bits 2 (0x04) and 14 (0x4000) are set, so
+        outputs[2+2] and outputs[14+2] are unspent
+      - outputs[4]: fe792b067e0061b01caab50f1b8e9c50a5057eb43c2d9563a4ee
+                    * fe792b067e: compact amount representation for 234925952 (2.35 BTC)
+                    * 00: special txout type pay-to-pubkey-hash
+                    * 61b01caab50f1b8e9c50a5057eb43c2d9563a4ee: address uint160
+      - outputs[16]: fe23290f00008c988f1a4a4de2161e0f50aac7f17e7f9555caa4
+                     * fe23290f00: compact amount representation for 110397 (0.001 BTC)
+                     * 00: special txout type pay-to-pubkey-hash
+                     * 8c988f1a4a4de2161e0f50aac7f17e7f9555caa4: address uint160
+      - height = 120891
+      - reference_height = 1000
+    """
+    # We only need one script pickler, which every instance of UnspentTransaction
+    # can use (there's no concurrency issues with picklers, and it needs to be
+    # available to the class anyway for deserialize).
+    _pickler = ScriptPickler()
+
+    def __init__(self, *args, **kwargs):
+        # Since we behave like a dictionary object, we implement the copy
+        # constructor, which requires copying meta information not contained
+        # within the dictionary itself.
+        if args and all(hasattr(args[0], x) for x in
+                ('version', 'is_coinbase', 'height', 'reference_height')):
+            other = args[0]
+        else:
+            other = None
+
+        # You can either specify the transaction, another UnspentTransaction
+        # object, or the metadata direction. Choose one.
+        a = 'transaction' in kwargs
+        b = other is not None
+        c = any(x in kwargs for x in (
+            'coinbase', 'version', 'height', 'reference_height'))
+        if a + b + c >= 2: # <-- yes, you can do this
+            raise TypeError(u"instantiate by either specifying the "
+                u"transaction directly, another %s, or its individual "
+                u"metadata; choose one" % self.__class__.__name__)
+
+        # Extract captured parameters from kwargs, starting with the transaction
+        # because its metadata are used as the default.
+        transaction = kwargs.pop('transaction', None)
+        if other is None:
+            other = transaction
+
+        version = kwargs.pop('version', getattr(other, 'version', 1))
+        is_coinbase = kwargs.pop('coinbase', getattr(other, 'is_coinbase', False))
+        height = kwargs.pop('height', getattr(other, 'height', 0))
+
+        # Reference heights are added with transaction version=2, so we do
+        # not extract that parameter unless version=2.
+        reference_height = getattr(other, 'reference_height', 0)
+        if version in (2,):
+            reference_height = kwargs.pop('reference_height', reference_height)
+
+        # Perform construction of the dictionary object (our superclass)
+        super(UnspentTransaction, self).__init__(*args, **kwargs)
+
+        # Store metadata
+        self.version          = version
+        self.is_coinbase      = is_coinbase
+        self.height           = height
+        self.reference_height = reference_height
+
+        # Add the transaction's outputs only if outputs are not separately
+        # specified (as is typically done if it is known in advance which
+        # outputs are not spent at time of creation).
+        if transaction is not None and not self:
+            for idx,output in enumerate(transaction.outputs):
+                self[idx] = output
+
+    def serialize(self):
+        # code&0x1: is_coinbase
+        # code&0x2: outputs[0] unspent
+        # code&0x4: outputs[1] unspent
+        # code>>3: N, the minimal length of bitvector in bytes, or N-1 if both
+        #   outputs[0] and outputs[1] are spent
+        code, bitvector = 0, 0
+        for idx in six.iterkeys(self):
+            bitvector |= 1 << idx
+        if not bitvector:
+            raise TypeError()
+        code |= bitvector & 0x3
+        bitvector >>= 2
+        bitvector = serialize_leint(bitvector)
+        bitvector_len = len(bitvector)
+        if not code:
+            bitvector_len -= 1
+        code |= bitvector_len << 2
+        code <<= 1
+        if self.is_coinbase:
+            code |= 1
+
+        result  = serialize_varint(self.version)
+        result += serialize_varint(code)
+        result += bitvector
+        for output in six.itervalues(self):
+            result += serialize_varint(compress_amount(output.amount))
+            result += self._pickler.dumps(output.contract)
+        result += serialize_varint(self.height)
+        if self.version in (2,):
+            result += serialize_varint(self.reference_height)
+        return result
+    @classmethod
+    def deserialize(cls, file_):
+        output_class = getattr(cls, 'get_output_class', lambda:
+                       getattr(cls, 'output_class', Output))()
+        kwargs = {}
+        kwargs['version'] = deserialize_varint(file_)
+
+        # See description of code, bitvector above.
+        code, bitvector = deserialize_varint(file_), 0
+        kwargs['coinbase'] = bool(code & 0x1)
+        code >>= 1
+        bitvector |= code & 0x3
+        code >>= 2
+        if not bitvector:
+            code += 1
+        if code:
+            bitvector |= deserialize_leint(file_, code) << 2
+        idx, items = 0, []
+        while bitvector:
+            if bitvector & 0x1:
+                items.append(
+                    (idx, output_class(
+                        decompress_amount(deserialize_varint(file_)),
+                        cls._pickler.load(file_))))
+            idx, bitvector = idx + 1, bitvector >> 1
+
+        kwargs['height'] = deserialize_varint(file_)
+        if kwargs['version'] in (2,):
+            kwargs['reference_height'] = deserialize_varint(file_)
+        return cls(items, **kwargs)
+
+    def __eq__(self, other):
+        # Compare metadata first, as it's probably less expensive
+        if any((self.version     != other.version,
+                self.is_coinbase != other.is_coinbase,
+                self.height      != other.height)):
+            return False
+        if self.version in (2,) and self.reference_height != other.reference_height:
+            return False
+        return super(UnspentTransaction, self).__eq__(other)
+    __ne__ = lambda a,b:not a==b
+    def __repr__(self):
+        return '%s%s' % (
+            self.__class__.__name__,
+            super(UnspentTransaction, self).__repr__()[10:])
+
+#
+# End of File
+#
---
+++ xunit/utxo.py
@@ -0,0 +1,248 @@
+# -*- coding: utf-8 -*-
+
+#
+# Copyright © 2012-2013 by its contributors. See AUTHORS for details.
+#
+# Distributed under the MIT/X11 software license, see the accompanying
+# file LICENSE or http://www.opensource.org/licenses/mit-license.php.
+#
+
+# Python standard library, unit-testing
+import unittest2
+
+# Python patterns, scenario unit-testing
+from python_patterns.unittest.scenario import ScenarioMeta, ScenarioTest
+
+from bitcoin.utxo import *
+from bitcoin.core import Output
+from bitcoin.destination import PubKeyHashId
+from bitcoin.utils import StringIO
+
+# ===----------------------------------------------------------------------===
+
+class TestUnspentTransaction(unittest2.TestCase):
+    def test_default_init(self):
+        utx = UnspentTransaction()
+        self.assertEqual(utx.version, 1)
+        self.assertFalse(utx.is_coinbase)
+        self.assertEqual(utx.height, 0)
+        self.assertEqual(len(utx), 0)
+        self.assertFalse(bool(utx))
+
+    def test_add_outputs(self):
+        utx = UnspentTransaction(version=1, coinbase=True, height=120891)
+        self.assertFalse(bool(utx))
+
+        utx[16] = Output(110397, PubKeyHashId(0xa4ca55957f7ef1c7aa500f1e16e24d4a1a8f988c).script)
+        self.assertEqual(utx.serialize(),
+            ('01' '09' '0040'
+             'fe23290f00' '008c988f1a4a4de2161e0f50aac7f17e7f9555caa4'
+             'fe3bd80100').decode('hex'))
+
+        utx[4] = Output(234925952, PubKeyHashId(0xeea463952d3cb47e05a5509c8e1b0fb5aa1cb061).script)
+        self.assertEqual(utx.serialize(),
+            ('01' '09' '0440'
+             'fe792b067e' '0061b01caab50f1b8e9c50a5057eb43c2d9563a4ee'
+             'fe23290f00' '008c988f1a4a4de2161e0f50aac7f17e7f9555caa4'
+             'fe3bd80100').decode('hex'))
+
+    def test_remove_outputs(self):
+        utx = UnspentTransaction((
+                (4, Output(234925952, PubKeyHashId(0xeea463952d3cb47e05a5509c8e1b0fb5aa1cb061).script)),
+                (16, Output(110397, PubKeyHashId(0xa4ca55957f7ef1c7aa500f1e16e24d4a1a8f988c).script)),
+            ), version=1, coinbase=True, height=120891)
+        self.assertEqual(utx.serialize(),
+            ('01' '09' '0440'
+             'fe792b067e' '0061b01caab50f1b8e9c50a5057eb43c2d9563a4ee'
+             'fe23290f00' '008c988f1a4a4de2161e0f50aac7f17e7f9555caa4'
+             'fe3bd80100').decode('hex'))
+        del utx[4]
+        self.assertEqual(utx.serialize(),
+            ('01' '09' '0040'
+             'fe23290f00' '008c988f1a4a4de2161e0f50aac7f17e7f9555caa4'
+             'fe3bd80100').decode('hex'))
+        del utx[16]
+        with self.assertRaises(TypeError):
+            utx.serialize()
+
+        utx = UnspentTransaction((
+                (4, Output(234925952, PubKeyHashId(0xeea463952d3cb47e05a5509c8e1b0fb5aa1cb061).script)),
+                (16, Output(110397, PubKeyHashId(0xa4ca55957f7ef1c7aa500f1e16e24d4a1a8f988c).script)),
+            ), version=1, coinbase=True, height=120891)
+        self.assertEqual(utx.serialize(),
+            ('01' '09' '0440'
+             'fe792b067e' '0061b01caab50f1b8e9c50a5057eb43c2d9563a4ee'
+             'fe23290f00' '008c988f1a4a4de2161e0f50aac7f17e7f9555caa4'
+             'fe3bd80100').decode('hex'))
+        del utx[16]
+        self.assertEqual(utx.serialize(),
+            ('01' '01' '04'
+             'fe792b067e' '0061b01caab50f1b8e9c50a5057eb43c2d9563a4ee'
+             'fe3bd80100').decode('hex'))
+        del utx[4]
+        with self.assertRaises(TypeError):
+            utx.serialize()
+
+UNSPENT_TRANSACTION = [
+    dict(version=1, coinbase=False, height=203998,
+         items = ((1, Output(60000000000, PubKeyHashId(0x351d7cf86bb3297fa5cf03c8e77f074e94156181).script)),),
+         string = ('01' '04'
+                   'fd5802' '00816115944e077fe7c803cfa57f29b36bf87c1d35'
+                   'fede1c0300').decode('hex')),
+    dict(version=1, coinbase=True, height=120891,
+         items = (
+             (4, Output(234925952, PubKeyHashId(0xeea463952d3cb47e05a5509c8e1b0fb5aa1cb061).script)),
+             (16, Output(110397, PubKeyHashId(0xa4ca55957f7ef1c7aa500f1e16e24d4a1a8f988c).script))),
+         string = ('01' '09' '0440'
+                   'fe792b067e' '0061b01caab50f1b8e9c50a5057eb43c2d9563a4ee'
+                   'fe23290f00' '008c988f1a4a4de2161e0f50aac7f17e7f9555caa4'
+                   'fe3bd80100').decode('hex')),
+    dict(version=2, coinbase=True, height=120891,
+         items = (
+             (4, Output(234925952, PubKeyHashId(0xeea463952d3cb47e05a5509c8e1b0fb5aa1cb061).script)),
+             (16, Output(110397, PubKeyHashId(0xa4ca55957f7ef1c7aa500f1e16e24d4a1a8f988c).script))),
+         string = ('02' '09' '0440'
+                   'fe792b067e' '0061b01caab50f1b8e9c50a5057eb43c2d9563a4ee'
+                   'fe23290f00' '008c988f1a4a4de2161e0f50aac7f17e7f9555caa4'
+                   'fe3bd80100' '00').decode('hex')),
+    dict(version=2, coinbase=True, height=120891, reference_height=1000,
+         items = (
+             (4, Output(234925952, PubKeyHashId(0xeea463952d3cb47e05a5509c8e1b0fb5aa1cb061).script)),
+             (16, Output(110397, PubKeyHashId(0xa4ca55957f7ef1c7aa500f1e16e24d4a1a8f988c).script))),
+         string = ('02' '09' '0440'
+                   'fe792b067e' '0061b01caab50f1b8e9c50a5057eb43c2d9563a4ee'
+                   'fe23290f00' '008c988f1a4a4de2161e0f50aac7f17e7f9555caa4'
+                   'fe3bd80100' 'fde803').decode('hex')),
+]
+
+class TestUnspentTransactionScenarios(unittest2.TestCase):
+    __metaclass__ = ScenarioMeta
+    class test_init_from_items(ScenarioTest):
+        scenarios = UNSPENT_TRANSACTION
+        def __test__(self, version, coinbase, height, items, string, **kwargs):
+            utx = UnspentTransaction(items, version=version, coinbase=coinbase,
+                                     height=height, **kwargs)
+            self.assertEqual(utx.version, version)
+            self.assertEqual(utx.is_coinbase, coinbase)
+            self.assertEqual(utx.height, height)
+            if 'reference_height' in kwargs:
+                self.assertEqual(utx.reference_height, kwargs['reference_height'])
+            for idx,output in items:
+                self.assertIn(idx, utx)
+                self.assertEqual(utx[idx], output)
+            self.assertEqual(utx.serialize(), string)
+
+    class test_init_from_dict(ScenarioTest):
+        scenarios = UNSPENT_TRANSACTION
+        def __test__(self, version, coinbase, height, items, string, **kwargs):
+            obj = dict(items)
+            utx = UnspentTransaction(obj, version=version, coinbase=coinbase,
+                                     height=height, **kwargs)
+            self.assertEqual(utx.version, version)
+            self.assertEqual(utx.is_coinbase, coinbase)
+            self.assertEqual(utx.height, height)
+            if 'reference_height' in kwargs:
+                self.assertEqual(utx.reference_height, kwargs['reference_height'])
+            for idx,output in items:
+                self.assertIn(idx, utx)
+                self.assertEqual(utx[idx], output)
+            self.assertEqual(utx.serialize(), string)
+
+    class test_init_from_other(ScenarioTest):
+        scenarios = UNSPENT_TRANSACTION
+        def __test__(self, version, coinbase, height, items, string, **kwargs):
+            obj = UnspentTransaction(items, version=version, coinbase=coinbase,
+                                     height=height, **kwargs)
+            utx = UnspentTransaction(obj)
+            self.assertEqual(utx.version, version)
+            self.assertEqual(utx.is_coinbase, coinbase)
+            self.assertEqual(utx.height, height)
+            if 'reference_height' in kwargs:
+                self.assertEqual(utx.reference_height, kwargs['reference_height'])
+            for idx,output in items:
+                self.assertIn(idx, utx)
+                self.assertEqual(utx[idx], output)
+            self.assertEqual(utx.serialize(), string)
+
+    class test_init_via_assignment(ScenarioTest):
+        scenarios = UNSPENT_TRANSACTION
+        def __test__(self, version, coinbase, height, items, string, **kwargs):
+            utx = UnspentTransaction()
+            utx.version = version
+            utx.is_coinbase = coinbase
+            utx.height = height
+            if 'reference_height' in kwargs:
+                utx.reference_height = kwargs['reference_height']
+            for idx,output in items:
+                utx[idx] = output
+            self.assertEqual(utx.version, version)
+            self.assertEqual(utx.is_coinbase, coinbase)
+            self.assertEqual(utx.height, height)
+            if 'reference_height' in kwargs:
+                self.assertEqual(utx.reference_height, kwargs['reference_height'])
+            for idx,output in items:
+                self.assertIn(idx, utx)
+                self.assertEqual(utx[idx], output)
+            self.assertEqual(utx.serialize(), string)
+
+    class test_init_from_serialization(ScenarioTest):
+        scenarios = UNSPENT_TRANSACTION
+        def __test__(self, version, coinbase, height, items, string, **kwargs):
+            utx = UnspentTransaction.deserialize(StringIO(string))
+            self.assertEqual(utx.version, version)
+            self.assertEqual(utx.is_coinbase, coinbase)
+            self.assertEqual(utx.height, height)
+            if 'reference_height' in kwargs:
+                self.assertEqual(utx.reference_height, kwargs['reference_height'])
+            for idx,output in items:
+                self.assertIn(idx, utx)
+                self.assertEqual(utx[idx], output)
+            self.assertEqual(utx.serialize(), string)
+
+    class test_equality(ScenarioTest):
+        scenarios = UNSPENT_TRANSACTION
+        def __test__(self, version, coinbase, height, items, string, **kwargs):
+            utx1 = UnspentTransaction.deserialize(StringIO(string))
+            utx2 = UnspentTransaction.deserialize(StringIO(string))
+            self.assertTrue(utx1 == utx2)
+            self.assertFalse(utx1 != utx2)
+            self.assertEqual(utx1.serialize(), utx2.serialize())
+
+            utx2.version = version - 1
+            self.assertFalse(utx1 == utx2)
+            self.assertTrue(utx1 != utx2)
+            self.assertNotEqual(utx1.serialize(), utx2.serialize())
+            utx2.version = version
+            self.assertTrue(utx1 == utx2)
+            self.assertEqual(utx1.serialize(), utx2.serialize())
+
+            utx2.is_coinbase = not coinbase
+            self.assertFalse(utx1 == utx2)
+            self.assertTrue(utx1 != utx2)
+            self.assertNotEqual(utx1.serialize(), utx2.serialize())
+            utx2.is_coinbase = coinbase
+
+            utx2.height = height + 1
+            self.assertFalse(utx1 == utx2)
+            self.assertTrue(utx1 != utx2)
+            self.assertNotEqual(utx1.serialize(), utx2.serialize())
+            utx2.height = height
+
+            if version in (2,):
+                utx2.reference_height = height - 1
+                self.assertFalse(utx1 == utx2)
+                self.assertTrue(utx1 != utx2)
+                self.assertNotEqual(utx1.serialize(), utx2.serialize())
+                utx2.reference_height = utx1.reference_height
+            self.assertTrue(utx1 == utx2)
+            self.assertEqual(utx1.serialize(), utx2.serialize())
+
+            del utx2[next(six.iterkeys(utx2))]
+            if utx2:
+                self.assertFalse(utx1 == utx2)
+                self.assertTrue(utx1 != utx2)
+                self.assertNotEqual(utx1.serialize(), utx2.serialize())
+
+#
+# End of File
+#
