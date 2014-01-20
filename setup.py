dd5a20d6db85eb6f616320d590bd3914810f7c3e
Mark Friedenbach <mark@friedenbach.org>
2014-01-20 11:46:11 -0800

    Rework setup.py to use setuptools and distribution files for package meta info.

--- setup.py
+++ setup.py
@@ -1,54 +1,34 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
-#
 # Copyright © 2012-2013 by its contributors. See AUTHORS for details.
-#
 # Distributed under the MIT/X11 software license, see the accompanying
 # file LICENSE or http://www.opensource.org/licenses/mit-license.php.
-#
 
 import os
 
-from distutils.core import setup
+from setuptools import setup, find_packages
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.md')).read()
+CHANGES = open(os.path.join(here, 'CHANGES.md')).read()
+requires = filter(lambda r:'libs/' not in r,
+    open(os.path.join(here, 'requirements.txt')).read().split())
+packages = filter(lambda p:not p.startswith('xunit'), find_packages())
 
 from bitcoin import get_version
-
-# Compile the list of packages available, because distutils doesn't have an
-# easy way to do this.
-packages, data_files = [], []
-root_dir = os.path.dirname(__file__)
-if root_dir:
-    os.chdir(root_dir)
-
-for dirpath, dirnames, filenames in os.walk('bitcoin'):
-    # Ignore dirnames that start with '.'
-    for i, dirname in enumerate(dirnames):
-        if dirname.startswith('.'): del dirnames[i]
-    if '__init__.py' in filenames:
-        pkg = dirpath.replace(os.path.sep, '.')
-        if os.path.altsep:
-            pkg = pkg.replace(os.path.altsep, '.')
-        packages.append(pkg)
-    elif filenames:
-        prefix = dirpath[len('bitcoin')+1:] # Strip "bitcoin/" or "bitcoin\"
-        for f in filenames:
-            data_files.append(os.path.join(prefix, f))
-
 version = get_version().replace(' ', '-')
-setup(name='python-bitcoin',
-    version=version,
-    description=
+setup(**{
+    'name': 'python-bitcoin',
+    'version': version,
+    'description':
         u"A collection of serialization and utility methods needed to "
         u"implement the bitcoin protocol.",
-    author='RokuSigma Inc.',
-    author_email='python-bitcoin@monetize.io',
-    url='http://www.github.com/monetizeio/python-bitcoin/',
-    download_url='http://pypi.python.org/packages/source/p/python-bitcoin/python-bitcoin-%s.tar.gz' % version,
-    package_dir={'bitcoin': 'bitcoin'},
-    packages=packages,
-    package_data={'bitcoin': data_files},
-    classifiers=[
+    'long_description': README + '\n\n' + CHANGES,
+    'author':       'Monetize.io Inc.',
+    'author_email': 'python-bitcoin@monetize.io',
+    'url':          'http://www.github.com/monetizeio/python-bitcoin/',
+    'download_url': 'http://pypi.python.org/packages/source/p/python-bitcoin/python-bitcoin-%s.tar.gz' % version,
+    'packages': packages,
+    'classifiers': [
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
@@ -57,21 +37,5 @@ setup(name='python-bitcoin',
         'Topic :: Office/Business :: Financial',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
-    install_requires=[
-        'bigfloat>=0.3.0a2',
-        'bitstring>=3.1.2',
-        'blist>=1.3.4',
-        'cdecimal>=2.3',
-        'ecdsa>=0.8',
-        'gmpy2>=2.0.0',
-        'isodate>=0.4.8',
-        'recordtype>=1.1',
-        'requests>=0.14.0',
-        'simplejson>=2.6.2',
-        'six>=1.3.0',
-    ],
-)
-
-#
-# End of File
-#
+    'install_requires': requires,
+})
